142ddb1798fa5be2e0758418d07df9dacd38d105
Nick Fong <132227268+nfch211@users.noreply.github.com>
2024-06-06 15:13:56 +0800

    Update Header.jsx
    add youtube link

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
 function Header () {
     return (<header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-    <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-      <svg className="bi me-2" width="0" height="0"><use xlinkHref="#bootstrap"></use></svg>
+    <a href="https://www.youtube.com/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+      <svg className="bi me-2" width="1" height="1"><use xlinkHref="#bootstrap"></use></svg>
       <span className="fs-4"> <img src="./svg/youtube.svg" style={{width:"50px"}} alt="" /> YouTube 🎃 👻 🥶 🏜️ R e a c t</span>
     </a>
         
