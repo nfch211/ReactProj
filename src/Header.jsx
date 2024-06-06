65616d6d1bdbe6c6a17ed505e2243c0ee29357f1
Nick Fong <132227268+nfch211@users.noreply.github.com>
2024-06-06 14:53:41 +0800

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,6 @@
 function Header () {
     return (<header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-    <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+    <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none" style={{paddingLeft: 0}}>
       <span className="fs-4"> <img src="./svg/youtube.svg" style={{width:"50px"}} alt="" /> YouTube 🎃 👻 🥶 🏜️ R e a c t</span>
     </a>
 
